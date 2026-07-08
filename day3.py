0aed6e91a43ba96e3ca260075b2a152d7a7eedba
Sagar R <sagar181721@gmail.com>
2026-07-08 10:09:24 +0530

    commit 3

---
+++ day3.py
@@ -0,0 +1,24 @@
+student = ("ram", "sam", "bheem")
+print(student[-2])
+## tuple is a colletcion used to store multiplt elements 
+numbers = (10, 20, 30, 40)
+print(numbers [-3])
+data = (1,2,3)
+data[0] = 100
+print(data)
+
+x = (1,2,3,2,1,1)
+print(x.count(1))
+print(x.count(2))
+
+x = ("sagar","pavan","suresh","sagar")
+print(x.count("sagar"))
+
+num = (10, 20, 30, 40, 50)
+print(num[1:4])
+
+x = {1,2,3,2,1,1,1}
+print(x)
+data = {1,2,3}
+data.add(4)
+print(data)
